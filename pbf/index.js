30edb85fe8aa5ac9e0270340d3d710e087ad52ab
Begerad, Stefan <dialog@swingbe.de>
2022-05-26 07:23:40 +0200

    feat: require pbf

--- pbf/index.js
+++ pbf/index.js
@@ -1,11 +1,21 @@
 const debug=require('debug')('pbf');
+const gtfsRt=require('../../proto2js/js/gtfs-rt.js');
+const Pbf = require('pbf');
 
 run().catch(err => {
-    debug('run: error')
+    debug('run: error');
     console.log(err)
 });
 
 async function run() {
-    debug('run started...')
-    debug('run done.')
+    debug('run started...');
+    const FeedMessage = gtfsRt.FeedMessage;
+    const buffer=[];
+    // read
+    debug('create pbf');
+    const pbf = new Pbf(buffer);
+    debug('create obj');
+    const obj = FeedMessage.read(pbf);
+    debug('use obj');
+    debug('run done.');
 }
